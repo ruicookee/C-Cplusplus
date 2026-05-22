225b04d62ce6837956599292d7f826c0de89fd05
ruicookee <cruikee@gmail.com>
2026-05-22 20:46:34 +0800

    initialising this c repo

---
+++ circle.c
@@ -0,0 +1,18 @@
+/*
+To calc area of circle in metres
+*/
+#include <stdio.h>
+
+#define PI 3.14159 //preprocessor directive too, but not a var, just text substitution
+// #define NAME value
+// or you can even do func-like macro #define SQUARE(x) ((x) * (x))
+// rmb () cause dumb text replacement
+
+int main() {
+    double area = 0.0, radius = 0.0; // double decimals with higher precision than float
+    printf("Enter Radius: ");
+    scanf("%lf", &radius); 
+    area = PI * radius * radius;
+    printf("Radius of %lf metres; area is %lf sqmetres\n", radius, area);
+    return 0;
+}
---
+++ hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+// first line is a preprocessor directive
+/* include means to copy and paste file there before compiling
+    <stdio.h> is a Standard Imput Output Header
+    it contains declarations for funcs*/
+
+int main() { // return type and name of func, and it takes no parameters
+    printf("Hello world\n");
+    return 0;
+}
+
